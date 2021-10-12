4cfcfe9351d5f2bbf8b7ab90ce14c44c9545a810
David Fójcik <dave.fojtas@gmail.com>
2021-10-12 10:20:42 +0200

    řízený chaos (mato direktor)

--- homeworks/kvadraticka_rovnice.py
+++ homeworks/kvadraticka_rovnice.py
@@ -1,28 +1,26 @@
 from math import sqrt
 
 
-def kvadra(a, b, c):
+def kvadra(a, b, c, vysledek1, vysledek2):
+    vysledky = (vysledek1, vysledek2)
     d = b * b - 4 * a * c
     print((-b + sqrt(d)))
     if d == 0:
-        x = float(-b / (2 * a))
-        return x
+        x1 = float(-b / (2 * a))
+        reseni = (x1, "none")
     elif d > 0:
         x1 = float((-b + sqrt(d)) / (2 * a))
         x2 = float((-b - sqrt(d)) / (2 * a))
-        return x1, x2
+        reseni = (x1, x2)
     elif d < 0:
-        return "do komplexnich cisel se mi ted kurva rozhodne nechce"
+        reseni = ("none", "none")
+    # kontrolní bod
+    if vysledky == reseni:
+        print("Korektně vypočteno")
+    else:
+        print("Někde nastala chyba")
 
 
-print(kvadra(2, -11, 14))
-print("3.5, 2")
-print("----------")
-
-print(kvadra(1, 2, -63))
-print("7, -9")
-print("----------")
-
-print(kvadra(2, 9, 0))
-print("0, -4.5")
-print("----------")
+kvadra(2, -11, 14, 3.5, 2)
+kvadra(1, 2, -63, 7, -9)
+kvadra(2, 9, 0, 0, -4.5)
