ce17e33d82291e1e5eb5779bff7e854847f97367
David Fójcik <dave.fojtas@gmail.com>
2021-10-12 09:35:21 +0200

    correct minor bug with parenthesis

--- homeworks/kvadraticka_rovnice.py
+++ homeworks/kvadraticka_rovnice.py
@@ -3,15 +3,16 @@ from math import sqrt
 
 def kvadra(a, b, c):
     d = b * b - 4 * a * c
-    x1 = 0
-    x2 = 0
+    print((-b + sqrt(d)))
     if d == 0:
-        x = -b / 2 * a
+        x = float(-b / (2 * a))
         return x
     elif d > 0:
-        x1 = (-b + sqrt(d)) / 2 * a
-        x2 = (-b - sqrt(d)) / 2 * a
+        x1 = float((-b + sqrt(d)) / (2 * a))
+        x2 = float((-b - sqrt(d)) / (2 * a))
         return x1, x2
     elif d < 0:
         return "do komplexnich cisel se mi ted kurva rozhodne nechce"
 
+
+print(kvadra(2, -11, 14))
