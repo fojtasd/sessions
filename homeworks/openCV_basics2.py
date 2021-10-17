f5dc0237fb0d19cf5d65cc0c1e2582b134c035fe
David Fójcik <dave.fojtas@gmail.com>
2021-10-17 14:13:02 +0200

    add correction

--- homeworks/openCV_basics2.py
+++ homeworks/openCV_basics2.py
@@ -12,24 +12,25 @@ class Rgb:
 
 
 def main():
-    color_changer(Rgb(51, 51, 255), Rgb(255, 153, 153))
+    color_changer(Rgb(0, 0, 153), Rgb(255, 255, 255))
 
 def color_changer(current_color: Rgb, demanded_color: Rgb):
     blank_image = np.zeros(shape=[512, 512, 3], dtype=np.uint8)
     blank_image[:] = (current_color.blue, current_color.green, current_color.red)
     while current_color != demanded_color:
-        if current_color.blue > 255:
-            current_color.blue = 0
+        cv.imshow("Colors", blank_image)
         if current_color.blue != demanded_color.blue:
             current_color.blue = current_color.blue + 1
-        if current_color.green > 255:
-            current_color.green = 0
+        if current_color.blue > 255:
+            current_color.blue = 0
         if current_color.green != demanded_color.green:
             current_color.green = current_color.green + 1
-        if current_color.red > 255:
-            current_color.red = 0
+        if current_color.green > 255:
+            current_color.green = 0
         if current_color.red != demanded_color.red:
             current_color.red = current_color.red + 1
+        if current_color.red > 255:
+            current_color.red = 0
         blank_image[:] = (current_color.blue, current_color.green, current_color.red)
         cv.waitKey(1)
         cv.imshow("Colors", blank_image)
