b43d7654f2364fac60e6a23966ac01a9318eeaec
David Fójcik <dave.fojtas@gmail.com>
2021-10-19 14:14:45 +0200

    add final commit of this homework

--- homeworks/numbers_in_range_by_step.py
+++ homeworks/numbers_in_range_by_step.py
@@ -1,19 +1,20 @@
-def numbers_in_range_by_step(a: int, b: int, step: int):
-    while a <= b:
+def numbers_in_range_by_step(a: float, b: float, step: float):
+    while a < b:
         a = a + step
         print(a)
 
 
-def numbers_in_range_by_amount_of_steps(a: int, b: int, amount_of_steps: int):
+def numbers_in_range_by_amount_of_steps(a: float, b: float, amount_of_steps: float):
     range = b - a
     step_size = range / amount_of_steps
-    while a <= b:
+    while a < b:
         a = a + step_size
         print(a)
 
 
 def main():
-    numbers_in_range_by_step()
+    numbers_in_range_by_step(0, 5, 0.1)
+    numbers_in_range_by_amount_of_steps(0, 10, 1)
 
 
 if __name__ == "__main__":
